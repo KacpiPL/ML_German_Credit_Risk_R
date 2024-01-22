f2be607074ebefcc06cb6853c21531947b7b305d
Kacper <kacpergruca@gmail.com>
2024-01-22 23:42:28 +0100

    Add script for xgboost

--- scripts/model1.R
+++ scripts/model1.R
@@ -5,19 +5,7 @@ library(xgboost)
 library(plyr)
 library(doParallel)
 library(pROC)
-
-rm(list=ls())
-
-df <- read.csv("./data/output/df.csv")
-
-df.train <- read.csv("./data/output/df.train.csv")
-df.test <- read.csv("./data/output/df.test.csv")
-
-# Delete the id from the dataset
-df <- df[,-1]
-df.train <- df.train[,-1]
-df.test <- df.test[,-1]
-
+library(ggplot2)
 
 cores=detectCores()
 registerDoParallel(cores=cores)
@@ -32,30 +20,8 @@ registerDoParallel(cores=cores)
 # # Let us also examine frequencies of the target variable inside the training and the testing set.
 # table(df.train$class)/length(df.train$class)
 # table(df.test$class)/length(df.test$class)
-# 
-# # Create the tree where all columns as x
-# model1.formula <- class ~ .
-# 
-# risk.tree1 <- 
-#   rpart(model1.formula, # model formula
-#         data = df.train, # data
-#         method = "class") # type of the tree: classification
-# 
-# risk.tree1
-# rpart.plot(risk.tree1)  
-# fancyRpartPlot(risk.tree1)
-
-
-
-# Scaling NOT needed:
-## decision tree
-## random forest
-## gmb
-## XGB
-## ADA - if applied to decision tree no needed, if svm needed
 
 # Start with xgb
-
 colnames(df)
 rm(list=ls())
 
@@ -74,119 +40,190 @@ df.test$class <- factor(df.test$class, levels = c(0, 1), labels = c("bad", "good
 table(df.train$class)
 table(df.test$class)
 
+rfe_columns <- read.csv("./data/output/rfe_columns.csv")
+rfe_columns <- c("class", rfe_columns$x)
+
+df.train <- df.train[, rfe_columns]
+df.test <- df.test[, rfe_columns]
+
 # colsample_bytree - # rule of thumb (number of predictors)
-sqrt(49)/49
+sqrt(33)/33
 
 # min_child_weight - 0.5 ... 0.1% of number of observations
-0.005 * 2000
-0.01 * 2000
+0.005 * 2000  # 10
+0.01 * 2000   # 20
 
-# Hyperpatrameter tuning
-# Example using caret for grid search
-# xgb_grid <- expand.grid(
-#   nrounds = seq(20, 100, 10),
-#   max_depth = seq(5, 9, 1),
-#   gamma = c(0, 1),
-#   eta = c(0.05, 0.1, 0.2),               # +
-#   colsample_bytree = c(0.5, 0.75, 1),    # +
-#   min_child_weight = c(10, 15, 20),      # +
-#   subsample = c(0.5, 0.75, 1)
-# )
-
-
-xgb_grid <- expand.grid(
-  nrounds = 100,
-  eta = c(0.01, 0.05, 0.1),
-  max_depth = c(3, 6, 9),
-  gamma = 0,
-  colsample_bytree = c(0.5, 0.75, 1),
-  min_child_weight = c(1, 2, 3),
-  subsample = c(0.5, 0.75, 1)
-)
-
-# parameters_xgb <- expand.grid(nrounds = seq(20, 80, 10),
-#                               max_depth = c(8),
-#                               eta = c(0.25), 
-#                               gamma = 1,
-#                               colsample_bytree = c(0.2),
-#                               min_child_weight = c(150),
-#                               subsample = 0.8)
+##### Hyperparameter tuning #####
+set.seed(12345678)
+# Tune nrounds
+xgb_grid <- expand.grid(nrounds = seq(20, 80, 10),
+                              max_depth = c(8),
+                              eta = c(0.25),
+                              gamma = 1,
+                              colsample_bytree = c(0.17),
+                              min_child_weight = c(10),
+                              subsample = 0.8)
 
 train_control <- trainControl(method = "cv", 
                               number = 5,
                               classProbs = TRUE,
                               summaryFunction = twoClassSummary)
 
-xgb_model <- train(class ~ .,
+xgb_model.1 <- train(class ~ .,
                    data = df.train,
                    method = "xgbTree",
                    trControl = train_control,
                    tuneGrid  = xgb_grid)
+xgb_model.1
 
-xgb_model
+xgb_model.1 %>% saveRDS("./data/output/xgb/xgb_model.1.rds")
 
-results <- xgb_model$results
+# best for nrunds = 70
 
-# Model evaluation
-predictions <- predict(xgb_model, df.test)
-conf_matrix <- confusionMatrix(predictions, df.test$class)
-print(conf_matrix)
+# Tune max_depth and min_child_weight
+xgb_grid <- expand.grid(nrounds = 70,
+                        max_depth = c(5, 15, 1),
+                        eta = c(0.25),
+                        gamma = 1,
+                        colsample_bytree = c(0.17),
+                        min_child_weight = seq(10, 200, 2),
+                        subsample = 0.8)
+set.seed(12345678)
+xgb_model.2 <- train(class ~ .,
+                     data = df.train,
+                     method = "xgbTree",
+                     trControl = train_control,
+                     tuneGrid  = xgb_grid)
+xgb_model.2
 
+xgb_model.2 %>% saveRDS("./data/output/xgb/xgb_model.2.rds")
+
+# The best for max_depth = 15, min_child_weight = 14
+
+# Tune colsample_bytree
+xgb_grid <- expand.grid(nrounds = 70,
+                        max_depth = 15,
+                        eta = c(0.25),
+                        gamma = 1,
+                        colsample_bytree = seq(0.05, 0.7, 0.01),
+                        min_child_weight = 14,
+                        subsample = 0.8)
+
+set.seed(12345678)
+xgb_model.3 <- train(class ~ .,
+                     data = df.train,
+                     method = "xgbTree",
+                     trControl = train_control,
+                     tuneGrid  = xgb_grid)
+xgb_model.3
+
+xgb_model.3 %>% saveRDS("./data/output/xgb/xgb_model.3.rds")
+# The best for colsample_bytree = 0.3
+
+# Tune subsample
+xgb_grid <- expand.grid(nrounds = 70,
+                        max_depth = 15,
+                        eta = c(0.25),
+                        gamma = 1,
+                        colsample_bytree = 0.3,
+                        min_child_weight = 14,
+                        subsample = seq(0.6, 0.9, 0.05))
+
+set.seed(12345678)
+xgb_model.4 <- train(class ~ .,
+                     data = df.train,
+                     method = "xgbTree",
+                     trControl = train_control,
+                     tuneGrid  = xgb_grid)
+xgb_model.4
+
+xgb_model.4 %>% saveRDS("./data/output/xgb/xgb_model.4.rds")
+# The best for subsample = 0.9
+
+# Change learning rate and number of trees
+xgb_grid <- expand.grid(nrounds = 140,
+                        max_depth = 15,
+                        eta = c(0.12),
+                        gamma = 1,
+                        colsample_bytree = 0.3,
+                        min_child_weight = 14,
+                        subsample = 0.9)
+
+set.seed(12345678)
+xgb_model.5 <- train(class ~ .,
+                     data = df.train,
+                     method = "xgbTree",
+                     trControl = train_control,
+                     tuneGrid  = xgb_grid)
+xgb_model.5
+
+xgb_model.5 %>% saveRDS("./data/output/xgb/xgb_model.5.rds")
+
+# Change learning rate and number of trees again
+xgb_grid <- expand.grid(nrounds = 280,
+                        max_depth = 15,
+                        eta = c(0.06),
+                        gamma = 1,
+                        colsample_bytree = 0.3,
+                        min_child_weight = 14,
+                        subsample = 0.9)
+
+set.seed(12345678)
+xgb_model.6 <- train(class ~ .,
+                     data = df.train,
+                     method = "xgbTree",
+                     trControl = train_control,
+                     tuneGrid  = xgb_grid)
+xgb_model.6
+
+xgb_model.6 %>% saveRDS("./data/output/xgb/xgb_model.6.rds")
+
+source("./scripts/getAccuracyAndGini.R")
+
+models <- c("1":"6")
+
+sapply(paste0("xgb_model.", models),
+       function(x) getAccuracyAndGini(model = get(x),
+                                      data = df.test,
+                                      target_variable = "class",
+                                      predicted_class = "good")
+)
+
+
+# xgb_model.6 has all values highest - we choose this
+
+# Calculate ROC
 ROC.train <- pROC::roc(df.train$class, 
-                       predict(xgb_model,
+                       predict(xgb_model.6,
                                df.train, type = "prob")[, "good"])
 
-ROC.test <- pROC::roc(df.test$class, 
-                       predict(xgb_model,
+ROC.test  <- pROC::roc(df.test$class, 
+                       predict(xgb_model.6,
                                df.test, type = "prob")[, "good"])
 
-plot(ROC.train)
-plot(ROC.test)
+cat("AUC for train = ", pROC::auc(ROC.train), 
+    ", Gini for train = ", 2 * pROC::auc(ROC.train) - 1, "\n", sep = "")
 
-predictions2 <- predict(xgb_model, df.train)
-conf_matrix <- confusionMatrix(predictions2, df.train$class)
-print(conf_matrix)
-
-# Feature importance
-importance_matrix <- xgb.importance(feature_names = colnames(train_data), model = xgb_model)
-xgb.plot.importance(importance_matrix)
+cat("AUC for test = ", pROC::auc(ROC.test), 
+    ", Gini for test = ", 2 * pROC::auc(ROC.test) - 1, "\n", sep = "")
 
 
-##### xgb na pca
-xgb_model_pca <- train(class ~ .,
-                   data = df_train_pca,
-                   method = "xgbTree",
-                   trControl = train_control,
-                   tuneGrid  = xgb_grid)
-
-results_pca <- xgb_model$results
-
-predictions_pca <- predict(xgb_model_pca, df_test_pca)
-conf_matrix_pca <- confusionMatrix(predictions_pca, df_test_pca$class)
-print(conf_matrix_pca)
-
-
-# xgb na rfe
-
-df.train.rfe <- df.train[, c("class", rfe_columns)]
-df.test.rfe <- df.test[, c("class", rfe_columns)]
-
-xgb_model_rfe <- train(class ~ .,
-                       data = df.train.rfe,
-                       method = "xgbTree",
-                       trControl = train_control,
-                       tuneGrid  = xgb_grid)
-
-results_rfe <- xgb_model_rfe$results
-
-predictions_rfe <- predict(xgb_model_rfe, df.test.rfe)
-
-conf_matrix_rfe <- confusionMatrix(predictions_rfe, df.test.rfe$class)
-
-
-print(conf_matrix)
-print(conf_matrix_pca)
-print(conf_matrix_rfe)
-
-
+# Chart of xgb ROC
+list(
+  ROC.train   = ROC.train,
+  ROC.test    = ROC.test
+) %>%
+  pROC::ggroc(alpha = 0.5, linetype = 1, size = 1) + 
+  geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), 
+               color = "grey", 
+               linetype = "dashed") +
+  labs(subtitle = paste0("Gini TRAIN: ",
+                         "xgb = ", 
+                         round(100 * (2 * auc(ROC.train) - 1), 1), "%, ",
+                         "Gini TEST: ",
+                         "xgb = ", 
+                         round(100 * (2 * auc(ROC.test) - 1), 1), "%, "
+                         )) +
+  theme_bw() + coord_fixed() +
+  scale_color_brewer(palette = "Paired")
 
