fd20ed5efc8397fc14283449422a07090103d6aa
Kacper <kacpergruca@gmail.com>
2024-02-05 23:51:56 +0100

    Modify variable choose to be reproducible (seed)

--- scripts/variable_choose.R
+++ scripts/variable_choose.R
@@ -2,40 +2,42 @@ library(mlbench)
 library(caret)
 library(recipes)
 
+rm(list=ls())
 
-# pierwsze podejście
-control <- trainControl(method="repeatedcv", number=10, repeats=3, verboseIter = TRUE)
-model <- train(class~., 
-               data=df.train, 
-               method="rf",
-               metric = "Accuracy", 
-               preProcess="scale", trControl=control)
+df.train <- read.csv("./data/output/df.train.csv")
+df.test <- read.csv("./data/output/df.test.csv")
 
-importance <- varImp(model, scale=FALSE)
-print(importance)
-plot(importance)
+# rfe requires factor as target variable
+df.train[,3] <- as.factor(df.train[,3])
 
-# drugie podejście - to które finalnie wybraliśmy
-control2 <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# Feature selection using Recursive Feature Elimination (RFE)
 
-results <- rfe(df.train[,c(1, 2, seq(4,length(df.train), 1))], df.train[,3], sizes=c(1:49), rfeControl=control2)
+# Define seeds for rfeControl
+# If we use doParallel packae and want to make our code reproducible we should pass seeds argument to rfeControl function
+# It requires a list where each element corresponds to a specific resampling iteration's seed, 
+# and the last element is a seed for the overall process.
+# Assuming 10-fold CV, we need a list of 11 elements: 10 for each fold and 1 for the overall process
+set.seed(123456789)
+numFolds <- 10
+seedsList <- lapply(1:numFolds, function(x) sample.int(1000, 49)) # Generate random seeds for each fold
+
+set.seed(123456789)
+seedsList[[length(seedsList) + 1]] <- sample.int(1000, 1) # Add one more seed for the overall process
+
+## Define rfeControl object
+control <- rfeControl(functions=rfFuncs, method="cv", number=10, seeds = seedsList)
+
+# # Perform feature selection with rfe
+# Target variable "class" is on the third position, so we set the parameters accordingly
+set.seed(123456789)
+results <- rfe(df.train[,c(1, 2, seq(4,length(df.train), 1))],      # predictor variables 
+               df.train[,3],                                        # target variable for prediction
+               sizes=c(1:49),                                       # the subset sizes to evaluate
+               rfeControl=control)
+
 predictors(results)
 plot(results, type=c("g", "o"))
 
 rfe_columns <- predictors(results)
 
 write.csv(rfe_columns, "./data/output/rfe_columns.csv")
-
-# PCA
-recipe_obj <- recipe(class~ ., data = df.train) %>%
-  step_scale(all_predictors()) %>%  # Standardize the predictors
-  step_pca(all_predictors(), threshold = 0.90)  # Perform PCA with 90% variance threshold
-
-recipe_obj <- prep(recipe_obj)
-
-df_train_pca <- bake(recipe_obj, new_data = df.train)
-df_test_pca <- bake(recipe_obj, new_data = df.test)
-
-model
-
-predictions <- predict(model, df_test_pca)
